803903521a2fe3432934c64ea2ac4022399fa7bc
Paulino8099 <dapa7928@gmail.com>
2022-01-26 15:16:41 -0400

    Versioń 15.5

--- JS/Body.js
+++ JS/Body.js
@@ -33,36 +33,39 @@ window.addEventListener("scroll", function () {
 });
 
 /**
-* ?botón para volver al inicio de la página
+* ?botón start...
 */
+// todas las configuraciones del botón para volver al inicio de la página
 window.addEventListener("scroll", function () {
   /**
-  *?función para calcular cuando debe de aparecer el botón para regresar al inicio de la página.
+  *?mostrar "btnStart"...
   */
+  //función para calcular cuando debe de aparecer el botón para regresar al inicio de la página.
   btnStart();
   function btnStart() {
     // Dividiendo el alto de todo el documento entre 4 para así extraer solo el 25% del documento.
     let btnAutomatic = document.documentElement.scrollHeight / 4;
-    // Cuando el vertical se haya movido un 25% de todo el documento el botón de inicio se va a mostrar. 
+    // Cuando la barra vertical se haya movido un 25% de todo el documento el botón de inicio se va a mostrar. 
     document.querySelector('.btn-start').classList.toggle('on', scrollY >= btnAutomatic);
   };
 
   /**
-  * ?función para calcular cuando el botón "home" tiene que moverse un poco hacia arriba para así evitar que tape o interfiera con el "footer".
+  * ?subir un poco "btnStart"...
   */
+  //función para calcular cuando el botón "home" tiene que moverse un poco hacia arriba para así evitar que tape o interfiera con el "footer".
   bottomBtnStart();
   function bottomBtnStart() {
     let footer = document.querySelector('.footer');
+    let docElem = document.documentElement;
 
-    let head = document.querySelector('.head').offsetHeight;
-    let estudios = document.querySelector('.estudios').offsetHeight;
-    let sobreMi = document.querySelector('.sobreMi').offsetHeight;
-    let contacto = document.querySelector('.contacto').offsetHeight;
+    console.log(docElem.scrollTop, docElem.scrollHeight)
 
-    document.documentElement.scrollTop >= head + estudios + sobreMi + contacto
+    // variables para detectar las diferentes alturas de la barra de escroleo vertical
+    let scroll486 = (docElem.scrollTop >= docElem.scrollHeight - (footer.offsetHeight) - 486);
+    let scroll780 = (docElem.scrollTop >= docElem.scrollHeight - (footer.offsetHeight) - 780);
 
-    // si la barra de navegación se ubica justamente en la posición donde se encuentra el footer, el btn de "home" se eleverá un poco (los "486" es porque  estos se le agregan a la barra de forma que desconozco)
-    if (document.documentElement.scrollTop >= document.documentElement.scrollHeight - (footer.offsetHeight) - 486) {
+    // si la barra de navegación vertical se ubica justamente en la posición donde se encuentra el footer, el btn de "home" se eleverá un poco hacia arriba (los "486" o los "780" es porque  estos se le agregan a la barra de escroleo de forma que desconozco, no sé porqué). Si el ancho del documento es menor que 650px, la función va a ler la segunda opción para ejecutar la función escrita (el btn se moverá hacia arriba)
+    if (scroll486 || (docElem.scrollWidth < 650 && scroll780)) {
       document.querySelector('.btn-start').style.bottom = `${footer.offsetHeight + 10}px`;
     } else {
       document.querySelector('.btn-start').style.bottom = `10px`;
@@ -71,8 +74,9 @@ window.addEventListener("scroll", function () {
 });
 
 /**
- *? botón para ocultar ventana de actualización.
-  */ 
+ *? ocultar ventana de actualización...
+  */
+//btn para ocultar la ventana de actualización
 document.querySelector(".btn_cancelar").addEventListener("click", () => {
   document.querySelector('.actualización').classList.toggle('on');
 });
